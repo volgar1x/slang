ef29a50e6e22db1aac63a9a2cdc7d46e2ce06777
Blackrush <blackrushx@gmail.com>
2015-12-26 13:14:03 +0100

    improve repl

--- repl/src/main/java/slang/repl/App.java
+++ repl/src/main/java/slang/repl/App.java
@@ -3,15 +3,33 @@ package slang.repl;
 import slang.SlangAssert;
 import slang.interpreter.Interpreter;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Antoine Chauvin
  */
 public final class App {
     private App() {}
 
-    public static void main(String[] args) {
-        Interpreter interpreter = new Interpreter(System.in, System.out, System.err);
-        SlangAssert.load(interpreter);
-        new REPL(interpreter).run();
+    public static void main(String[] args_) throws IOException {
+        List<String> args = Arrays.asList(args_);
+
+        REPL repl;
+
+        if (args.contains("-i")) {
+            // read code from file
+            String filename = args.get(args.indexOf("-i") + 1);
+            repl = REPL.fromFile(Paths.get(filename), args.contains("-v"));
+        } else {
+            // read code from stdin and provide some niceties over it
+            repl = REPL.mount(new Interpreter(System.in, System.out, System.err));
+        }
+
+        SlangAssert.load(repl.getInterpreter());
+
+        repl.run();
     }
 }
--- repl/src/main/java/slang/repl/REPL.java
+++ repl/src/main/java/slang/repl/REPL.java
@@ -11,6 +11,8 @@ import slang.parser.Parser;
 import slang.tokenizer.Tokenizer;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * @author Antoine Chauvin
@@ -19,12 +21,14 @@ public final class REPL {
     private final Interpreter interpreter;
     private final MacroExpander macroExpander;
     private final Parser parser;
+    private final boolean verbose;
     private boolean alive;
 
-    public REPL(Interpreter interpreter) {
+    private REPL(Interpreter interpreter, MacroExpander macroExpander, Parser parser, boolean verbose) {
         this.interpreter = interpreter;
-        this.macroExpander = new MacroExpander(this.interpreter);
-        this.parser = new Parser(new Tokenizer(new Pretty(interpreter.getStandardOutput(), interpreter.getStandardInput())));
+        this.macroExpander = macroExpander;
+        this.parser = parser;
+        this.verbose = verbose;
 
         this.interpreter.register("kill", (FunctionInterface) (context, arguments) -> {
             this.alive = false;
@@ -32,13 +36,37 @@ public final class REPL {
         });
     }
 
+    public static REPL mount(Interpreter interpreter) {
+        InputStream stdin = new Pretty(interpreter.getStandardOutput(), interpreter.getStandardInput());
+
+        return new REPL(interpreter,
+                new MacroExpander(interpreter),
+                new Parser(new Tokenizer(stdin)), /*verbose*/true);
+    }
+
+    public static REPL fromFile(Path path, boolean verbose) throws IOException {
+        Interpreter interpreter = new Interpreter(System.in, System.out, System.err);
+
+        return new REPL(interpreter,
+                new MacroExpander(interpreter),
+                new Parser(new Tokenizer(Files.newInputStream(path))),
+                verbose);
+    }
+
+    public Interpreter getInterpreter() {
+        return interpreter;
+    }
+
     public ExpressionInterface run() {
         this.alive = true;
         ExpressionInterface result = NilExpression.NIL;
-        while (this.alive) {
+        while (this.alive && parser.hasNext()) {
             try {
                 result = interpreter.evaluate(macroExpander.evaluate(parser.next()));
-                interpreter.getStandardOutput().println(Inspector.inspect(result));
+
+                if (verbose) {
+                    interpreter.getStandardOutput().println(Inspector.inspect(result));
+                }
             } catch (SlangException e) {
                 interpreter.getStandardError().println(e.getMessage());
             }
@@ -52,10 +80,16 @@ public final class REPL {
 
         String buf;
         int pos;
+        boolean eof;
 
         Pretty(PrintStream stdout, InputStream stdin) {
             this.stdout = stdout;
             this.stdin = stdin;
+        }
+
+        @Override
+        public int available() throws IOException {
+            return eof ? -1 : Integer.MAX_VALUE;
         }
 
         @Override
@@ -66,8 +100,10 @@ public final class REPL {
 
                 buf = reader.readLine();
                 pos = 0;
+                eof = buf == null;
             }
-            return buf.codePointAt(pos++);
+
+            return !eof && pos < buf.length() ? buf.codePointAt(pos++) : -1;
         }
     }
 }
