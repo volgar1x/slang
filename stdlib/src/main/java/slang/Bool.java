bf9dfdee8e7a3e02a7fcbc383130d9495768ce19
Blackrush <blackrushx@gmail.com>
2016-01-05 09:51:43 +0100

    some testers

--- stdlib/src/main/java/slang/Bool.java
+++ stdlib/src/main/java/slang/Bool.java
@@ -11,6 +11,16 @@ public final class Bool {
         Stdlib.loadFn(context, SAtom.of("not"), Bool::not, true);
         Stdlib.loadFn(context, SAtom.of("<="), Bool::gte, true);
         Stdlib.loadFn(context, SAtom.of("nil?"), Bool::nilTest, true);
+        Stdlib.loadFn(context, SAtom.of("many?"), Bool::manyTest, true);
+        Stdlib.loadFn(context, SAtom.of("list?"), Bool::listTest, true);
+        Stdlib.loadFn(context, SAtom.of("map?"), Bool::mapTest, true);
+        Stdlib.loadFn(context, SAtom.of("set?"), Bool::setTest, true);
+        Stdlib.loadFn(context, SAtom.of("vec?"), Bool::vecTest, true);
+        Stdlib.loadFn(context, SAtom.of("int?"), Bool::intTest, true);
+        Stdlib.loadFn(context, SAtom.of("dec?"), Bool::decTest, true);
+        Stdlib.loadFn(context, SAtom.of("str?"), Bool::strTest, true);
+        Stdlib.loadFn(context, SAtom.of("atom?"), Bool::atomTest, true);
+        Stdlib.loadFn(context, SAtom.of("fn?"), Bool::fnTest, true);
     }
 
     public static Object eq(EvaluationContextInterface context, SList arguments) {
@@ -52,11 +62,54 @@ public final class Bool {
         return ((Comparable) lhs).compareTo(rhs) >= 0 ? SAtom.of("true") : SList.nil;
     }
 
+    private static Object _bool(boolean b) {
+        return b ? SAtom.of("true") : SList.nil;
+    }
+
     public static Object nilTest(EvaluationContextInterface context, SList arguments) {
-        if (arguments.head() == SList.nil) {
-            return SAtom.of("true");
-        } else {
-            return SList.nil;
-        }
+        return _bool(arguments.head() == SList.nil);
+    }
+
+    public static Object manyTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SMany);
+    }
+
+    public static Object listTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SList);
+    }
+
+    public static Object mapTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SMap);
+    }
+
+    public static Object setTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SSet);
+    }
+
+    public static Object vecTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SVector);
+    }
+
+    public static Object intTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof Byte
+                || arguments.head() instanceof Short
+                || arguments.head() instanceof Integer
+                || arguments.head() instanceof Long);
+    }
+
+    public static Object decTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof Float || arguments.head() instanceof Double);
+    }
+
+    public static Object strTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof String);
+    }
+
+    public static Object atomTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SAtom);
+    }
+
+    public static Object fnTest(EvaluationContextInterface context, SList arguments) {
+        return _bool(arguments.head() instanceof SFunction);
     }
 }
